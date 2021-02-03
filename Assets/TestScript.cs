d23bb729abe19e589fe9b0d6c92809c477646e00
JayBarton <damonjaybarton@gmail.com>
2021-02-03 09:38:38 -0500

    Transisitions are now two way.

---
+++ Assets/TestScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Vector2 playerPosition = transform.InverseTransformPoint(other.transform.position);
+
+        if(playerPosition.x > 0)
+        {
+            Debug.Log("Out");
+        }
+        else
+        {
+            Debug.Log("In");
+        }
+    }
+}
--- Assets/TransitionScript.cs
+++ Assets/TransitionScript.cs
@@ -18,11 +18,27 @@ public class TransitionScript : MonoBehaviour
         
     }
 
-    void OnTriggerEnter2D(Collider2D col)
+   /* void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("GameObject1 collided with " + col.name);
         previous.SetActive(false);
         next.SetActive(true);
         gameObject.SetActive(false);
+    }*/
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Vector2 playerPosition = transform.InverseTransformPoint(other.transform.position);
+
+        if (playerPosition.x > 0)
+        {
+            previous.SetActive(false);
+            next.SetActive(true);
+        }
+        else
+        {
+            previous.SetActive(true);
+            next.SetActive(false);
+        }
     }
 }
