6c496623b0688e24ae093098077f429a5833c266
JD2895 <j.davis.2895@gmail.com>
2021-03-09 23:06:41 -0500

    - implemented first part of aim assist, the laser visual now works the way hook aiming/firing will eventually work

--- Assets/Scripts/Player/VisualAimAssist.cs
+++ Assets/Scripts/Player/VisualAimAssist.cs
@@ -25,6 +25,17 @@ public class VisualAimAssist : MonoBehaviour
     PlayerControls controls;
     float horizontalInput;
     float verticalInput;
+    Vector2 aimingDirection;
+
+    [Header("Aim Assist")]
+    public LayerMask castingLayers;
+    public float castDistance;
+    public List<float> circleSizes = new List<float>();
+
+    // Debug Visuals
+    Vector3 debugCircleOrigin = new Vector3();
+    float debugCircleSize = 0;
+    bool drawDebugCircle = false;
 
     private void Awake()
     {
@@ -109,12 +120,22 @@ public class VisualAimAssist : MonoBehaviour
         if (DebugOptions.hookFireVarient == HookFireVariant.OneHook)// && PlayerMovement_v3.SnapOctDirection(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) != Vector2.zero)
         {
             rightLine.enabled = false;
-            Vector2 aimingDirection = PlayerMovement_v3.SnapOctDirection(horizontalInput, verticalInput);
+            leftLine.enabled = false;
+
+            aimingDirection = PlayerMovement_v3.SnapOctDirection(horizontalInput, verticalInput);
             if (aimingDirection != Vector2.zero)
             {
                 rightLine.enabled = true;
                 rightLine.SetPosition(0, this.transform.position);
                 rightLine.SetPosition(1, this.transform.position + ((Vector3)aimingDirection * fadeDistance));
+
+                AimingData targetAimData = GetTargetPosition();
+                if (targetAimData.validTarget)
+                {
+                    leftLine.enabled = true;
+                    leftLine.SetPosition(0, targetAimData.targetPosition);
+                    leftLine.SetPosition(1, targetAimData.targetPosition + ((Vector3)aimingDirection * fadeDistance * -0.5f));
+                }
             }
         }
         else if (DebugOptions.hookFireVarient != HookFireVariant.None)
@@ -126,5 +147,61 @@ public class VisualAimAssist : MonoBehaviour
             leftLine.SetPosition(0, this.transform.position);
             leftLine.SetPosition(1, this.transform.position + (new Vector3(-1f, 1f, 0f) * fadeDistance));
         }
+    }
+
+    public AimingData GetTargetPosition()
+    {
+        Vector3 targetPosition = new Vector3(0,0,0);
+        bool validTarget = false;
+
+        foreach(float castSize in circleSizes)
+        {
+            Vector3 aimStartOffset = this.transform.position + (0.5f * (Vector3)aimingDirection);
+            RaycastHit2D castHit = Physics2D.CircleCast(aimStartOffset, castSize, aimingDirection, castDistance, castingLayers);
+            
+            /*// Debug drawing circles
+            debugCircleOrigin = aimStartOffset;
+            debugCircleSize = castSize;
+            drawDebugCircle = true; //*/
+            
+            if (castHit.collider != null)
+            {
+                // The following if statement makes sure that the first thing the cast hits is on the gorund layer
+                // If the thing we hit isn't the ground layer, we try again in the next cast first before breaking out
+                if (castHit.collider.CompareTag("CameraEdge") || castHit.collider.CompareTag("Hazard"))
+                {
+                    validTarget = false;
+                }
+                else
+                {
+                    targetPosition = castHit.point;
+                    validTarget = true;
+                    break;
+                }
+            }
+        }
+
+        return new AimingData(targetPosition, validTarget);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (drawDebugCircle)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(debugCircleOrigin, debugCircleSize);
+        }
+    }
+
+    public struct AimingData
+    {
+        public AimingData(Vector3 newTargetPosition, bool newValidTarget)
+        {
+            targetPosition = newTargetPosition;
+            validTarget = newValidTarget;
+        }
+
+        public Vector3 targetPosition;
+        public bool validTarget;
     }
 }
